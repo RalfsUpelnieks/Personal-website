75858a029a4ac1f21123e16786823f52fdeaabf8
Ralfs Upelnieks <ralfs.upelnieks@gmail.com>
2023-07-08 13:30:27 +0300

    Improvements for mobile use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
+	var	$window = $(window), 
+        $body = $('body'),
 		$header = $('#header'),
         $homeSection = $("#home-section");
 
 	// Play initial animations on page load.
-        $window.on('load', function() {
-            window.setTimeout(function() {
-                $homeSection.removeClass('hidden');
-            }, 100);
-        });
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $homeSection.removeClass('hidden');
+        }, 100);
+    });
 
 	// Header.
-        $window.on("scroll", function(){
-            if(this.scrollY >= $homeSection.height() * 0.9){
-                if($header.hasClass('alt')){
-                    $header.removeClass('alt');
-                }
-            } else if(!$header.hasClass('alt')){
-                $header.addClass('alt');
+    $window.on("scroll", function(){
+        if(this.scrollY >= $homeSection.height() * 0.9){
+            if($header.hasClass('alt')){
+                $header.removeClass('alt');
             }
-        });
+        } else if(!$header.hasClass('alt')){
+            $header.addClass('alt');
+        }
+    });
 })(jQuery);
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
